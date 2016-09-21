1e4b264f3f83c25b4ae097089b2b3f8d2d1cc7ad
somename <someemail>
2016-09-21 14:45:44 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -8,5 +8,34 @@ public class TestBowling {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void testFrame_2_4_firstScoreEquals_2() throws BowlingException{
+		Frame frm = new Frame(2,4);
+		int firstScore = frm.getFirstThrow();
+		assertEquals(2,firstScore);
+	}
+	
+	@Test
+	public void testFrame_2_4_secondScoreEquals_4() throws BowlingException{
+		Frame frm = new Frame(2,4);
+		int secondScore = frm.getSecondThrow();
+		assertEquals(4,secondScore);
+	}
 
+	@Test (expected = BowlingException.class)
+	public void testFrame_11_0_throwBowlingException() throws BowlingException{
+		Frame frm = new Frame(11,0);
+	}
+	
+	@Test (expected = BowlingException.class)
+	public void testFrame_5_7_throwBowlingException() throws BowlingException{
+		Frame frm = new Frame(5,7);
+	}
+	
+	@Test (expected = BowlingException.class)
+	public void testFrame_minus1_7_throwBowlingException() throws BowlingException{
+		Frame frm = new Frame(-1,7);
+	}
+	
 }
