1ac05e2c59a68e64c1548b671cf48fbc88487d96
somename <someemail>
2016-09-21 15:31:49 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -147,6 +147,24 @@ public class TestBowling {
 		int number = bg.score();
 		assertEquals(88,number);
 	}
+	
+	// example : [10,0] [4,6] [7,2] [3,6] [4,4] [5,3] [3,3] [4,5] [8,1] [2,6]
+	@Test
+	public void testScoreWithSpareFollowdBySpare_example4case_equals_103() throws BowlingException{
+		BowlingGame bg = new BowlingGame();
+		bg.addFrame(new Frame(10,0));
+		bg.addFrame(new Frame(4,6));
+		bg.addFrame(new Frame(7,2));
+		bg.addFrame(new Frame(3,6));
+		bg.addFrame(new Frame(4,4));
+		bg.addFrame(new Frame(5,3));
+		bg.addFrame(new Frame(3,3));
+		bg.addFrame(new Frame(4,5));
+		bg.addFrame(new Frame(8,1));
+		bg.addFrame(new Frame(2,6));
+		int number = bg.score();
+		assertEquals(103,number);
+	}
 }
 
 
